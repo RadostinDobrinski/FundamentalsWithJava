5ab075d17afd2646e42479a27b7c2bcbc110e55f
radostindobrinski <venya9@abv.bg>
2022-01-24 18:30:03 +0200

    P04, P05

---
+++ src/BasicSyntaxConditionalStatement/P06StrongNumber.java
@@ -0,0 +1,11 @@
+package BasicSyntaxConditionalStatement;
+
+import java.util.Scanner;
+
+public class P06StrongNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}
